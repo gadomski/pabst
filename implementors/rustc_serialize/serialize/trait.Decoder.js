61629fa78b043fc73b78fbd45f072de03280c8ab
Pete Gadomski <pete.gadomski@gmail.com>
2015-11-25 12:09:28 +0000

    rebuild pages at 73e16fc

--- implementors/rustc_serialize/serialize/trait.Decoder.js
+++ implementors/rustc_serialize/serialize/trait.Decoder.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['docopt'] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Decoder.html' title='rustc_serialize::serialize::Decoder'>Decoder</a> for <a class='struct' href='docopt/struct.Decoder.html' title='docopt::Decoder'>Decoder</a>",];
+implementors['docopt'] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Decoder.html' title='rustc_serialize::serialize::Decoder'>Decoder</a> for <a class='struct' href='docopt/struct.Decoder.html' title='docopt::Decoder'>Decoder</a>",];implementors['toml'] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Decoder.html' title='rustc_serialize::serialize::Decoder'>Decoder</a> for <a class='struct' href='toml/struct.Decoder.html' title='toml::Decoder'>Decoder</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
